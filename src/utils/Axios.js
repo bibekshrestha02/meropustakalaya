5a606535a6a6cc0e9115418414f1e8fcf8f247e9
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-14 09:48:53 +0545

    fixing url

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'http://localhost:4000/api/v1/',
+  baseURL: 'https://meropustakalaya-api.herokuapp.com/api/v1/',
   headers: {
     'x-auth-token': localStorage.getItem('token')
       ? localStorage.getItem('token')
