5b5fd94ea566f93ec1b8495ea462ec63f6a899bb
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-14 14:29:05 +0545

    added sub rating number in rating

--- src/assets/RatingAsset.jsx
+++ src/assets/RatingAsset.jsx
@@ -3,14 +3,17 @@ import ReactStars from 'react-rating-stars-component';
 import style from './style.module.scss';
 export default function Star({ value, isEdit, size, ...props }) {
   return (
-    <ReactStars
-      count={5}
-      size={size || 15}
-      activeColor='#ef5354'
-      edit={isEdit}
-      value={value}
-      className={style.rating}
-      {...props}
-    />
+    <div style={{ display: 'flex' }}>
+      <ReactStars
+        count={5}
+        size={size || 15}
+        activeColor='#ef5354'
+        edit={isEdit}
+        value={value}
+        className={style.rating}
+        {...props}
+      />
+      <sub style={{ fontSize: 10 }}>{value}</sub>
+    </div>
   );
 }
