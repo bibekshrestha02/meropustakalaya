abb5f57fe500c945b6949e4d842d9d85a8796948
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-15 10:37:54 +0545

    made custom hooks and templetes to fetched api

---
+++ src/customHooks/useFetchApi.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+export const useFetchApi = (url, ref) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (ref.current) {
+      (async () => {
+        Axios.all(url)
+          .then(
+            Axios.spread((...responses) => {
+              let resData = [];
+              responses.forEach((res) => {
+                return resData.push(res.data.data);
+              });
+              setData(resData);
+              setLoading(false);
+            })
+          )
+          .catch((error) => {
+            setError(error);
+          });
+      })();
+    }
+    return () => {
+      ref.current = false;
+    };
+  }, [url, ref]);
+
+  return { data, loading, error };
+};
---
+++ src/templetes/FetchApiTemplete.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import LoadingComponent from '../component/models/LoadingComponent';
+import ErrorPageContainer from '../component/models/ErrorPageContainer';
+import MainTempletes from './MainTempletes';
+const FetchApiTemplete = ({ loading, children, error }) => {
+  if (loading) {
+    return (
+      <MainTempletes>
+        <LoadingComponent />
+      </MainTempletes>
+    );
+  } else if (error) {
+    return (
+      <MainTempletes>
+        <ErrorPageContainer />
+      </MainTempletes>
+    );
+  }
+  return <MainTempletes>{children}</MainTempletes>;
+};
+
+export default FetchApiTemplete;
