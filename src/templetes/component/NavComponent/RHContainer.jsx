5284231a80e7009bd871bd5b2b6a89a8f4670339
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-16 11:07:55 +0545

    fixing nav bugs

--- src/templetes/component/NavComponent/RHContainer.jsx
+++ src/templetes/component/NavComponent/RHContainer.jsx
@@ -37,7 +37,7 @@ export default function RHContainer({ loginHandler, membershipHandler }) {
 
   return (
     <div className={style.roleContainer}>
-      {isLogin ? authNavRHC : unAuthNavRHC}
+      {isLogin && role ? authNavRHC : unAuthNavRHC}
     </div>
   );
 }
