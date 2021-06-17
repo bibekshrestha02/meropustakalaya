0538ef4eab8d9e5f55f22cf58b77f577efec6f67
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-17 11:13:38 +0545

    fixing bugs

--- src/pages/users/SavePage.jsx
+++ src/pages/users/SavePage.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import CardTemplete from '../../templetes/CardTemplete';
 import style from './style/style.module.scss';
-import GetFetchPageTemplete from '../../templetes/GetFetchPageTemplete';
-
+import Axios from '../../utils/Axios';
+import { useFetchApi } from '../../customHooks/useFetchApi';
+import FetchApiTemplete from '../../templetes/FetchApiTemplete';
 export default function HomePage() {
+  const getSaveBooks = () => {
+    return Axios.get('/users/saves');
+  };
+  const isComponentMounted = React.useRef(true);
+  const { data, loading, error, setData } = useFetchApi(
+    [getSaveBooks()],
+    isComponentMounted
+  );
+  const bookMarkToogler = (id) => {
+    let res = data[0];
+    let newState = res.filter((e) => e._id !== id);
+    setData([newState]);
+  };
+
   return (
-    <GetFetchPageTemplete urls={['/users/saves/']}>
-      {({ data, setData }) => {
-        const bookMarkToogler = (id) => {
-          let res = data[0];
-          let newState = res.filter((e) => e._id !== id);
-          setData([newState]);
-        };
-        return (
-          <div className={style.saveContainer}>
-            <CardTemplete data={data[0]} sm toogler={bookMarkToogler} />
-          </div>
-        );
-      }}
-    </GetFetchPageTemplete>
+    <FetchApiTemplete loading={loading} error={error}>
+      <div className={style.saveContainer}>
+        <CardTemplete data={data[0]} toogler={bookMarkToogler} />
+      </div>
+    </FetchApiTemplete>
   );
 }
