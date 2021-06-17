e2b856bf55c2a9d1f05f7515368e143e8aa25701
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-17 11:11:59 +0545

    restyled bookmark

--- src/assets/BookmarkAsset.jsx
+++ src/assets/BookmarkAsset.jsx
@@ -3,17 +3,15 @@ import style from './style.module.scss';
 import { IoBookmarkOutline, IoBookmark } from 'react-icons/io5';
 import { useSelector, useDispatch } from 'react-redux';
 import { saveBook } from '../store/action/authAction';
+import Axios from '../utils/Axios';
 export default function BookMark({ bookId, toogler }) {
   const dispatch = useDispatch();
   const isLogin = useSelector((state) => state.auth.isLogin);
   let isSave = useSelector((state) => state.auth.savedBooks.includes(bookId));
   const bookmarkHanlder = async () => {
-    try {
-      await dispatch(saveBook(bookId));
-      toogler && toogler(bookId);
-    } catch (error) {
-      console.log(error);
-    }
+    dispatch(saveBook(bookId));
+    toogler && toogler(bookId);
+    await Axios.post(`/users/saves/${bookId}`);
   };
 
   return isLogin ? (
