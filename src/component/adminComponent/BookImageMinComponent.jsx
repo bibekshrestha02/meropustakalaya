2ff2dad764a89400dad3c52f2f54e981cf1474b6
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-17 12:51:20 +0545

    implemented imageLoading

--- src/component/adminComponent/BookImageMinComponent.jsx
+++ src/component/adminComponent/BookImageMinComponent.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import style from './style.module.scss';
+import ImageLoadingComponent from '../models/ImageLoadingComponent';
 export default function BookImageMinComponent({ imgUrl }) {
+  const [isLoaded, setLoad] = React.useState(false);
+  const imageLoadingHandler = () => {
+    setLoad(true);
+  };
   return (
     <div className={style.photoContainer}>
-      <img src={imgUrl} alt='bookImg' className={style.image} />
+      <ImageLoadingComponent isImageLoaded={isLoaded} size={30} />
+      <img
+        src={imgUrl}
+        alt='bookImg'
+        className={style.image}
+        loading='lazy'
+        onError={imageLoadingHandler}
+        onLoad={imageLoadingHandler}
+      />
     </div>
   );
 }
--- src/component/models/ImageLoadingComponent.jsx
+++ src/component/models/ImageLoadingComponent.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { GoBook } from 'react-icons/go';
 import style from './style.module.scss';
-export default function ImageLoadigCo({ isImageLoaded }) {
+export default function ImageLoadigCo({ isImageLoaded, size }) {
   return isImageLoaded ? null : (
     <div className={style.imageLoadingContainer}>
-      <GoBook color='gray' size={100} />
+      <GoBook color='gray' size={size ? size : 100} />
     </div>
   );
 }
